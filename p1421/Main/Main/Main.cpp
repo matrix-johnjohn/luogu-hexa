c09616f9952de279180ea60c0825d7c369ee59d2
kimo <1596903229@qq.com>
2026-07-03 23:10:28 +0800

    '添加p1421题解'

---
+++ p1421/Main/Main/Main.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+
+
+int main()
+{
+    double i,j;
+
+    cin>>i>>j;
+
+    double total=i+j * 0.1;
+
+    cout<<floor(total/1.9);
+
+    return 0;
+}
