090a3b7e7a47c4ffe6d96a4fe57e12afc225ee9a
kimo <1596903229@qq.com>
2026-07-04 11:09:43 +0800

    添加p3954题解

---
+++ p3954/Main/Main/Main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+
+    cin>>a>>b>>c;
+
+    int h=a*20/100,t=b*30/100,r=c*50/100;
+
+    cout<<h+t+r;
+
+    return 0;
+}
