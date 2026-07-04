177499b4fceed1b2639de46150a2c0d628a75216
kimo <1596903229@qq.com>
2026-07-04 11:22:51 +0800

    添加p5706题解

---
+++ p5706/Main.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+#include <format>
+
+using namespace std;
+
+int main()
+{
+	double t;int n;
+	cin >> t >> n;
+	cout << format("{:.3f}", t / n) << endl;
+	cout << n * 2;
+	return 0;
+}
