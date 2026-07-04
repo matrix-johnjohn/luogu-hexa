4672c7cef3396f2af325391b747757a1bbc53b4e
kimo <1596903229@qq.com>
2026-07-04 11:26:32 +0800

    添加p5708题解

---
+++ p5708/Main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+#include <format>
+
+using namespace std;
+
+int main()
+{
+	double a, b, c;
+
+	cin>>a>>b>>c;
+
+	double p=(a+b+c)/2;
+
+	double sum=sqrt(p*(p-a)*(p-b)*(p-c));
+	
+	cout << format("{:.1f}",sum);
+
+
+	return 0;
+}
