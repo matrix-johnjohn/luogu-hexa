34070dde1281a2cff90deaeb83d204bb937567f5
kimo <1596903229@qq.com>
2026-07-04 11:27:26 +0800

    添加p5709题解

---
+++ p5709/Main/Main/Main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double m,t,s;
+    cin>>m>>t>>s;
+
+    if (t == 0 || m - ceil(s / t)<0)
+    {
+        cout << 0;
+        return 0;
+    };
+
+    cout << m - ceil(s/t);
+
+    
+
+    return 0;
+}
